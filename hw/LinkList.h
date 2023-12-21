88baf23b694055844bf019a42eb435997866e234
Liu-Annan <3561007877@qq.com>
2023-12-21 15:06:09 +0800

    modified:   hw/LinkList.h

---
+++ hw/LinkList.h
@@ -0,0 +1,20 @@
+#pragma once
+
+struct linknode
+{
+	int data;
+	struct linknode* next;
+};
+//初始化链表
+struct linknode* init_linklist();
+//尾插法
+void insertafter(struct linknode* header, int oldval, int newval);
+//删除值为value的结点
+void remove(struct linknode* header, int delval);
+//遍历
+void traverse(struct linknode* header);
+//销毁
+void destroy(struct linknode* header);
+//清空
+void clear(struct linknode* header);
+
