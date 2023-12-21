ca0f2132639703f4785f501b38ffc9e1f78c694a
Liu-Annan <3561007877@qq.com>
2023-12-21 20:43:42 +0800

    ✨ feat:complete

--- hw/LinkList.c
+++ hw/LinkList.c
@@ -30,13 +30,126 @@ struct linknode* init_linklist()
         return header;
     }
 }
-//尾插法
-void insertafter(struct linknode* header, int oldval, int newval);
+//头插法
+void inserthead(struct linknode* header, int oldval, int newval)
+{
+    if(header==NULL)
+    {
+        return;
+    }
+    //需要两个辅助指针
+    struct linknode* ppreview=header;
+    struct linknode* pcurrent=ppreview->next;
+    while(pcurrent!=NULL)
+    {
+        //找到要插入结点的地址就跳出循环
+        if(pcurrent==oldval)
+        {
+            break;
+        }
+        //调整指针
+        ppreview=pcurrent;
+        pcurrent=pcurrent->next;
+    }
+    if(pcurrent==NULL)
+    {
+        return;
+    }
+    else
+    {
+        //创建新的结点
+        struct linknode* newnode = (struct linknode*)malloc(sizeof(struct linknode)); 
+        newnode->data=newval;
+        newnode->next=NULL;
+        //将结点插入链表
+        ppreview=newnode;
+        newnode->next=pcurrent;
+    }
+}
 //删除值为value的结点
-void remove(struct linknode* header, int delval);
+void remove(struct linknode* header, int delval)
+{
+    if(header==NULL)
+    {
+        return;
+    }
+    //需要两个辅助指针
+    struct linknode* ppreview=header;
+    struct linknode* pcurrent=ppreview->next;
+    while(pcurrent!=NULL)
+    {
+        //找到所要删除的结点就跳出循环
+        if(pcurrent->data==delval)
+        {
+            break;
+        }
+        //调整指针
+        ppreview=pcurrent;
+        pcurrent=pcurrent->next;
+    }
+    if(pcurrent==NULL)
+    {
+        return;
+    }
+    else
+    {
+        //删除结点
+        ppreview->next=pcurrent->next;
+        free(pcurrent);
+        pcurrent=NULL;
+    }
+}
 //遍历
-void traverse(struct linknode* header);
+void traverse(struct linknode* header)
+{
+    if(header==NULL)
+    {
+        return;
+    }
+    //需要一个辅助指针
+    struct linknode* pcurrent=header->next;
+    while(pcurrent!=NULL)
+    {
+        printf("%d ",pcurrent->data);
+        pcurrent=pcurrent->next;
+    }
+}
 //销毁
-void destroy(struct linknode* header);
+void destroy(struct linknode* header)
+{
+    if(header==NULL)
+    {
+        return;
+    }
+    //需要一个辅助指针
+    struct linknode* pcurrent=header;
+    while(pcurrent!=NULL)
+    {
+        //保存当前结点的下一个结点的地址
+        struct linknode* pnext=pcurrent->next;
+        //释放当前结点
+        free(pcurrent);
+        //调整指针
+        pcurrent=pnext;
+    }
+}
 //清空
-void clear(struct linknode* header);
+void clear(struct linknode* header)
+{
+    if(header==NULL)
+    {
+        return;
+    }
+    //需要一个辅助指针
+    struct linknode* pcurrent=header->next;
+    while(pcurrent!=NULL)
+    {
+        //保存当前结点的下一个结点的地址
+        struct linknode* pnext=pcurrent->next;
+        //释放当前结点
+        free(pcurrent);
+        //调整指针
+        pcurrent=pnext;
+    }
+    header->next=NULL;
+}
--- hw/LinkList.h
+++ hw/LinkList.h
@@ -9,8 +9,8 @@ struct linknode
 };
 //初始化链表
 struct linknode* init_linklist();
-//尾插法
-void insertafter(struct linknode* header, int oldval, int newval);
+//头插法
+void inserthead(struct linknode* header, int oldval, int newval);
 //删除值为value的结点
 void remove(struct linknode* header, int delval);
 //遍历
