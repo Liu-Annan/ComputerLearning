22c1fee251a05c065160a130b83e2c221096b3ab
Liu-Annan <3561007877@qq.com>
2023-12-21 16:04:24 +0800

    modified:   hw/LinkList.c 	modified:   hw/LinkList.h

--- hw/LinkList.c
+++ hw/LinkList.c
@@ -1,7 +1,31 @@
 #include "linklist.h"
 
 //初始化链表
-struct linknode* init_linklist();
+struct linknode* init_linklist()
+{
+    struct linknode* header = (struct linknode*)malloc(sizeof(struct linknode));
+    header->data=-1;
+    header->next=NULL;
+
+    //尾部指针
+    struct linknode* pRear = header;
+    int val=-1;
+    while(true)
+    {
+        printf("请输入要插入的数据:\\n");
+        scanf("%d",&val);
+        if(val==-1)
+        {
+            break;
+        }
+        //创造新结点
+        struct linknode* newnode = (struct linknode*)malloc(sizeof(struct linknode)); 
+        newnode->data=val;
+        newnode->next=NULL;
+        
+
+    }
+}
 //尾插法
 void insertafter(struct linknode* header, int oldval, int newval);
 //删除值为value的结点
--- hw/LinkList.h
+++ hw/LinkList.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 struct linknode
 {
 	int data;
